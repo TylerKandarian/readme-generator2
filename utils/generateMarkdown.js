78589dfd9f29f534e04160145c4f22e85b2b0578
tylerkandarian <tyler.kand2@gmail.com>
2022-10-31 15:57:30 -0400

    fixing readme format

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -2,35 +2,34 @@ const generateMarkdown = data => {
     return `# ${data.title}
     ![Github licence](http://img.shields.io/badge/license-${data.license}-blue.svg)
     
-    ## Description 
-    ${data.description}
+## Description 
+  ${data.description}
 
-    ## Table of Contents
-    - [Installation](#installation)
-    - [Usage](#usage)
-    - [License](#license)
-    - [Contributing](#contributing)
-    - [Tests](#tests)
-    - [Questions](#questions)
+## Table of Contents
+- [Installation](#installation)
+- [Usage](#usage)
+- [License](#license)
+- [Contributing](#contributing)
+- [Tests](#tests)
+- [Questions](#questions)
     
-    ## Installation 
-    ${data.install}
+## Installation 
+  ${data.install}
 
-    ## Usage 
-    ${data.usage}
+## Usage 
+  ${data.usage}
 
-    ## License 
-    This project is license under ${data.license}
+## License 
+  This project is license under ${data.license}
 
-    ## Contributing 
-    ${data.contributors}
+## Contributing 
+  ${data.contributors}
 
-    ## Tests
-    ${data.test}
-    
-    ## Questions
+## Tests
+  ${data.test}
+
+## Questions
     If you have any questions about this project, please email at ${data.email}. You can view more of my work at https://github.com/${data.github}.
-  `;
-  }
+  `;}
 
   module.exports = generateMarkdown;
